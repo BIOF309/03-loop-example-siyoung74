84dc2d2731ecd02b9954f886864b53e9e9c60be4
siyoung74 <31741329+siyoung74@users.noreply.github.com>
2017-10-23 18:00:04 -0400

    Add files via upload

---
+++ 03-loop-example-siyoung74.py
@@ -0,0 +1,9 @@
+# git-github.com-FAES-BIOF-03-loop-example-siyoung74
+# example; yeast genome size of each chromosome
+size = {"I":5.7, "II":4.6, "III":3.5}
+for key, val in size.items():
+    print("Chromosome " + key + ": " + str(val)+" Mb")
+
+#size = (5.7,4.6,3.5)
+#for key, val in enumerate(size):
+#    print("Chromosome " + str(key) + ": " + str(val)+" Mb")
